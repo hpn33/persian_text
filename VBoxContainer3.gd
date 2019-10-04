3840eb115277c6606d96394bad6f180fbdb90258
hamed pishdad <hamed.hpn332@gmail.com>
2019-10-04 16:32:11 +0330

    change input var

--- VBoxContainer3.gd
+++ VBoxContainer3.gd
@@ -5,7 +5,7 @@ func _ready():
 	var sample_text := ")متن 1: این متن »نمونه‌ای« است برای بررسی \"صحت حروف‌چینی\" _*+گودوت‌انجین+*_ ("
 	var tx := ".   سلام. hello to you بهترین الله nice جیگر"
 	
-	set_text(tx)
+	set_text(sample_text)
 
 
 
